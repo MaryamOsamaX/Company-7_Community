c5a89a89e2608535c4274fd1f30e19bb683ec2fd
MaryamOsamaX <MaryamOsamaX@users.noreply.github.com>
2019-03-16 15:11:46 +0300

    Master
    Master

---
+++ Community/src/main/java/Company7/Community/App.java
@@ -0,0 +1,13 @@
+package Company7.Community;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
