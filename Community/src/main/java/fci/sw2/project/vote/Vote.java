b44615ce5f0dd7c5a6cf195f1a6e4450345b5918
EsraaIB <EsraaIB@users.noreply.github.com>
2019-04-17 18:36:24 +0300

    Vote
    Vote

---
+++ Community/src/main/java/fci/sw2/project/vote/Vote.java
@@ -0,0 +1,27 @@
+package fci.sw2.project.vote;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+@Entity
+public class Vote {
+	
+	private Key key;
+	private String type;
+	@EmbeddedId
+	public Key getKey() {
+		return key;
+	}
+	
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
---
+++ Community/src/main/java/fci/sw2/project/vote/VoteRepository.java
@@ -0,0 +1,7 @@
+package fci.sw2.project.vote;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VoteRepository extends CrudRepository<Vote ,Key> {
+
+}
